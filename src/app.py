19679f60a1ee264ad8b899250713df0e631b9d36
Bikatr7 <Tetralon07@gmail.com>
2024-05-02 13:34:23 -0600

    undid htaccess changes, moved error pages to under templates and switched to headerless template for those
    added a headerless template
    
    did proper pathing for app.py for error pages

--- src/app.py
+++ src/app.py
@@ -4,10 +4,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
+## Routes
+
+## Home Page
+
 @app.route('/')
 def home():
     app.logger.debug("Serving Home Page")
     return render_template('home.html')
+
+## Okisouchi Pages
 
 @app.route('/okisouchi')
 def okisouchi():
@@ -23,6 +29,20 @@ def tos():
 def privacy_policy():
     app.logger.debug("Serving Privacy Policy Page")
     return render_template('okisouchi/privacypolicy/privacypolicy.html')
+
+## Error Handlers
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('/error_pages/404.shtml'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('/error_pages/500.shtml'), 500
+
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template('/error_pages/403.shtml'), 403
 
 if(__name__ == '__main__'):
     app.run(debug=True)
