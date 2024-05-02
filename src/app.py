f9ee00314765bf07072adf3b557fab8a224ee290
Bikatr7 <Tetralon07@gmail.com>
2024-05-01 21:04:40 -0600

    adjust  hta acces and added some more detailed logged for debugging

--- src/app.py
+++ src/app.py
@@ -1,23 +1,28 @@
+import logging
 from flask import Flask, render_template
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def home():
+    app.logger.debug("Serving Home Page")
     return render_template('home.html')
 
 @app.route('/okisouchi')
 def okisouchi():
+    app.logger.debug("Serving Okisouchi Page")
     return render_template('okisouchi/okisouchi.html')
 
 @app.route('/okisouchi/tos/')
 def tos():
+    app.logger.debug("Serving TOS Page")
     return render_template('okisouchi/tos/tos.html')
 
 @app.route('/okisouchi/privacypolicy/')
 def privacy_policy():
+    app.logger.debug("Serving Privacy Policy Page")
     return render_template('okisouchi/privacypolicy/privacypolicy.html')
 
-## for development
-if(__name__ == '__main__'):
+if __name__ == '__main__':
     app.run(debug=True)
