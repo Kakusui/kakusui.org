f46e5283b829755c7e9fbee4ea743ba72440322d
Bikatr7 <Tetralon07@gmail.com>
2024-05-02 14:27:26 -0600

    added more debugger info

--- src/app.py
+++ src/app.py
@@ -34,14 +34,20 @@ def privacy_policy():
 
 @app.errorhandler(404)
 def page_not_found(e):
+    app.logger.error("Page Not Found" + str(e))
+    app.logger.debug("Serving 404 Page")
     return render_template('/error_pages/404.shtml'), 404
 
 @app.errorhandler(500)
 def internal_server_error(e):
+    app.logger.error("Internal Server Error" + str(e))
+    app.logger.debug("Serving 500 Page")
     return render_template('/error_pages/500.shtml'), 500
 
 @app.errorhandler(403)
 def forbidden(e):
+    app.logger.error("Forbidden" + str(e))
+    app.logger.debug("Serving 403 Page")
     return render_template('/error_pages/403.shtml'), 403
 
 if(__name__ == '__main__'):
