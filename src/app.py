2b554a65eadd1ebfc4eaeb44cd19419b1532f600
Bikatr7 <Tetralon07@gmail.com>
2024-05-02 21:53:11 -0600

    added "copyright" headers and changed the footer slightly

--- src/app.py
+++ src/app.py
@@ -1,3 +1,7 @@
+## Copyright Kakusui LLC 2024 (https://kakusui.org) (https://github.com/Kakusui)
+## Use of this source code is governed by a GNU General Public License v3.0
+## license that can be found in the LICENSE file.
+
 ## built-in libraries
 from logging.handlers import RotatingFileHandler
 
