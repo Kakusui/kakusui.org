d518518c5b5ea6de84618cc823765367f9e79388
Bikatr7 <Bikatr7@proton.me>
2024-06-02 12:11:18 -0600

    refactored kairyou page

--- frontend/src/pages/KairyouPage.tsx
+++ frontend/src/pages/KairyouPage.tsx
@@ -1,21 +1,28 @@
+// react things
 import React, { useEffect, useMemo, useState, useRef } from "react";
 import { useForm } from "react-hook-form";
+
+// custom things
 import { getURL } from "../utils";
+import Turnstile from "../components/Turnstile";
+
+// chakra things
 import {
     Box, Button, Flex, FormErrorMessage, FormControl, FormLabel, IconButton, Link, Text, Textarea, useToast, Stack, Center
 } from "@chakra-ui/react";
 import { ArrowUpIcon, DownloadIcon } from "@chakra-ui/icons";
-import Turnstile from "../components/Turnstile";
 
-type FormInput = {
-    textToPreprocess: string,
-    replacementsJson: string,
+type FormInput = 
+{
+    textToPreprocess: string;
+    replacementsJson: string;
 };
 
-type ResponseValues = {
-    errorLog: string,
-    preprocessedText: string,
-    preprocessingLog: string,
+type ResponseValues = 
+{
+    errorLog: string;
+    preprocessedText: string;
+    preprocessingLog: string;
 };
 
 function KairyouPage() 
@@ -25,7 +32,6 @@ function KairyouPage()
     const [turnstileToken, setTurnstileToken] = useState<string | null>(null);
     const [isBlacklistedDomain, setBlacklistedDomain] = useState(false);
     const [resetTurnstile, setResetTurnstile] = useState(false);
-
     const { register, handleSubmit, setValue, formState: { isSubmitting, errors } } = useForm<FormInput>();
     const [response, setResponse] = useState<ResponseValues>();
     const toast = useToast();
@@ -34,228 +40,180 @@ function KairyouPage()
     {
         const warmUpAPI = async () => 
         {
-            try 
+            try
             {
-                await fetch(getURL("/v1/kairyou"), {
-                    method: "GET",
-                });
-            } 
-            catch (error) 
+                await fetch(getURL("/v1/kairyou"), { method: "GET" });
+            }
+            catch
             {
                 // handle error silently
             }
         };
-
         warmUpAPI();
     }, []);
 
-    useEffect(() => {
+    useEffect(() => 
+    {
         const currentDomain = window.location.hostname;
-        // this just actually hides the turnstile on any none kakusui.org domain
-        if(currentDomain !== "kakusui.org")
-            {
-            setBlacklistedDomain(true);
-        }
+        setBlacklistedDomain(currentDomain !== "kakusui.org");
     }, []);
 
-    const onTurnstileVerify = (token: string) => {
+    const onTurnstileVerify = (token: string) => 
+    {
         setTurnstileToken(token);
     };
 
-    const onSubmit = async (data: FormInput) => {
-        setResetTurnstile(false);
-    
-        // you cannot use forms here on the pages.dev domain
-        if (window.location.hostname === "kakusui-org.pages.dev") {
-            toast({
-                title: "Access Denied",
-                description: "This domain is not for end user usage, please use kakusui.org",
-                status: "error",
-                duration: 5000,
-                isClosable: true,
-            });
-            return;
-        }
-    
+    const showToast = (title: string, description: string, status: "success" | "error") => 
+    {
+        toast({
+            title,
+            description,
+            status,
+            duration: 5000,
+            isClosable: true,
+        });
+    };
 
-        // only verify Turnstile on kakusui.org
-        if (!turnstileToken && window.location.hostname === "kakusui.org") {
-            toast({
-                title: "Verification failed",
-                description: "Please complete the verification",
-                status: "error",
-                duration: 5000,
-                isClosable: true,
-            });
-            return;
-        }
-    
-        // Validate JSON
-        try 
+    const validateJSON = (json: string) => 
+    {
+        try
         {
-            JSON.parse(data.replacementsJson);
-        } 
-        catch (e) 
-        {
-            toast({
-                title: "Invalid JSON",
-                description: "Please provide a valid JSON format.",
-                status: "error",
-                duration: 5000,
-                isClosable: true,
-            });
-            return;
+            JSON.parse(json);
+            return true;
         }
-    
-        try {
-            // Do not verify Turnstile on any other domain than kakusui.org
-            if (window.location.hostname === "kakusui.org") 
-            {
-                const verificationResponse = await fetch(getURL("/verify-turnstile"), {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ token: turnstileToken })
-                });
-    
-                const verificationResult = await verificationResponse.json();
-    
-                if (!verificationResult.success) {
-                    throw new Error("Turnstile verification failed");
-                }
-            }
-    
-            const response = await fetch(getURL("/proxy/kairyou"), {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data)
-            });
-    
-            const result = await response.json();
-    
-            if (!response.ok) 
-            {
-                throw new Error(result.message || "An unknown error occurred");
-            }
-    
-            setResponse(result);
-        } catch (error) {
-            console.error("Error Occurred:", error);
-    
-            let description = "An error occurred.";
-            if (error instanceof SyntaxError) {
-                description = "Invalid JSON format.";
-            } else {
-                description = (error as Error).message;
-            }
-    
-            toast({
-                title: "An error occurred.",
-                description: description,
-                status: "error",
-                duration: 5000,
-                isClosable: true,
-            });
-        } finally {
-            setResetTurnstile(true); // Reset the Turnstile after processing
+        catch
+        {
+            return false;
         }
     };
-    
+
+    const handleVerification = async () => 
+    {
+        const verificationResponse = await fetch(getURL("/verify-turnstile"), {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ token: turnstileToken }),
+        });
+
+        const verificationResult = await verificationResponse.json();
+        return verificationResult.success;
+    };
+
+    const onSubmit = async (data: FormInput) => 
+    {
+        setResetTurnstile(false);
+
+        if(window.location.hostname === "kakusui-org.pages.dev")
+        {
+            showToast("Access Denied", "This domain is not for end user usage, please use kakusui.org", "error");
+            return;
+        }
+
+        if(!turnstileToken && window.location.hostname === "kakusui.org")
+        {
+            showToast("Verification failed", "Please complete the verification", "error");
+            return;
+        }
+
+        if(!validateJSON(data.replacementsJson))
+        {
+            showToast("Invalid JSON", "Please provide a valid JSON format.", "error");
+            return;
+        }
+
+        try
+        {
+            if(window.location.hostname === "kakusui.org" && !(await handleVerification()))
+            {
+                throw new Error("Turnstile verification failed");
+            }
+
+            const response = await fetch(getURL("/proxy/kairyou"), {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data),
+            });
+
+            const result = await response.json();
+            if(!response.ok) throw new Error(result.message || "An unknown error occurred");
+
+            setResponse(result);
+        }
+        catch(error)
+        {
+            console.error("Error Occurred:", error);
+            showToast("An error occurred.", (error as Error).message || "An error occurred.", "error");
+        }
+        finally
+        {
+            setResetTurnstile(true);
+        }
+    };
 
     const onUpload = async (event: React.ChangeEvent<HTMLInputElement>) => 
     {
         const file = event.target.files?.[0];
-        if (!file) 
-        {
-            return;
-        }
+        if(!file) return;
 
         const input = await file.text();
-        switch (file.type) 
-        {
-            case "text/plain":
-                setValue("textToPreprocess", input);
-                return;
-            case "application/json":
-                setValue("replacementsJson", input);
-                return;
-            default:
-                throw new Error("Invalid file type");
-        }
+        const fileTypeHandlers: { [key: string]: (input: string) => void } = {
+            "text/plain": (input) => setValue("textToPreprocess", input),
+            "application/json": (input) => setValue("replacementsJson", input),
+        };
+
+        fileTypeHandlers[file.type]?.(input);
     };
 
     const downloadOutput = (input: 'preprocessing_log' | 'preprocessedText') => 
     {
-        let content = '';
-        if (input === 'preprocessing_log') 
-        {
-            content = response?.preprocessingLog || '';
-        } 
-        else if (input === 'preprocessedText') 
-        {
-            content = response?.preprocessedText || '';
-        } 
-        else 
-        {
-            throw new Error("Invalid input type");
-        }
-
-        if (!content) 
-        {
-            return;
-        }
+        const content = input === 'preprocessing_log' ? response?.preprocessingLog : response?.preprocessedText;
+        if(!content) return;
 
         const blob = new Blob([content], { type: "text/plain" });
         const url = URL.createObjectURL(blob);
-        const link = document.createElement("a") as HTMLAnchorElement;
-        link.download = input + '.txt';
+        const link = document.createElement("a");
+        link.download = `${input}.txt`;
         link.href = url;
         link.click();
     };
 
-    const memoizedTurnstile = useMemo(() => {
-        return <Turnstile siteKey="0x4AAAAAAAbu-SlGyNF03684" onVerify={onTurnstileVerify} resetKey={resetTurnstile} />;
-    }, [resetTurnstile]);
+    const memoizedTurnstile = useMemo(() => 
+    (
+        <Turnstile siteKey="0x4AAAAAAAbu-SlGyNF03684" onVerify={onTurnstileVerify} resetKey={resetTurnstile} />
+    ), [resetTurnstile]);
 
     return (
         <>
-            {/* Hidden file inputs. Will allow us to use styled buttons */}
-            <input onChange={onUpload} ref={textRef} type={'file'} accept={'.txt'} hidden />
-            <input onChange={onUpload} ref={jsonRef} type={'file'} accept={'.json'} hidden />
+            <input onChange={onUpload} ref={textRef} type="file" accept=".txt" hidden />
+            <input onChange={onUpload} ref={jsonRef} type="file" accept=".json" hidden />
             <form onSubmit={handleSubmit(onSubmit)}>
                 <Flex flex={1} gap={2}>
                     <FormControl isInvalid={!!errors.textToPreprocess} flex={1}>
-                        <FormLabel>Text Input <IconButton variant={'ghost'} size={'xl'}
-                            onClick={() => textRef.current?.click()}
-                            aria-label={'Upload Text'} icon={<ArrowUpIcon />} /> </FormLabel>
-                        <Textarea rows={20} size='sm' {...register("textToPreprocess", { required: true })} />
-                        <FormErrorMessage>
-                            {errors.textToPreprocess && "Text Input is required"}
-                        </FormErrorMessage>
+                        <FormLabel>
+                            Text Input
+                            <IconButton variant="ghost" size="xl" onClick={() => textRef.current?.click()} aria-label="Upload Text" icon={<ArrowUpIcon />} />
+                        </FormLabel>
+                        <Textarea rows={20} size="sm" {...register("textToPreprocess", { required: true })} />
+                        <FormErrorMessage>{errors.textToPreprocess && "Text Input is required"}</FormErrorMessage>
                     </FormControl>
                     <FormControl isInvalid={!!errors.replacementsJson} flex={1}>
-                        <FormLabel>JSON Input <IconButton variant={'ghost'} size={'xl'}
-                            onClick={() => jsonRef.current?.click()}
-                            aria-label={'Upload JSON'} icon={<ArrowUpIcon />} /> </FormLabel>
-                        <Textarea rows={20} size='sm' {...register("replacementsJson", { required: true })} />
-                        <FormErrorMessage>
-                            {errors.replacementsJson && "JSON Input is required"}
-                        </FormErrorMessage>
+                        <FormLabel>
+                            JSON Input
+                            <IconButton variant="ghost" size="xl" onClick={() => jsonRef.current?.click()} aria-label="Upload JSON" icon={<ArrowUpIcon />} />
+                        </FormLabel>
+                        <Textarea rows={20} size="sm" {...register("replacementsJson", { required: true })} />
+                        <FormErrorMessage>{errors.replacementsJson && "JSON Input is required"}</FormErrorMessage>
                     </FormControl>
                 </Flex>
 
                 <Button
-                    mb={4} mt={4} width='100%' type="submit"
-                    bg={'orange.400'}
-                    color={'white'}
-                    _hover={{
-                        bg: 'orange.500',
-                    }}
+                    mb={4} mt={4} width="100%" type="submit"
+                    bg="orange.400" color="white"
+                    _hover={{ bg: "orange.500" }}
                     isLoading={isSubmitting}
-                >Submit</Button>
+                >
+                    Submit
+                </Button>
             </form>
 
             {!isBlacklistedDomain && (
@@ -266,53 +224,47 @@ function KairyouPage()
 
             {response && (
                 <>
-                    <Flex mt={17} gap='2'>
-
+                    <Flex mt={17} gap={2}>
                         <Box flex={1}>
-                            <Text mb='8px'>Preprocessing Log <IconButton onClick={() => downloadOutput("preprocessing_log")} variant={'ghost'}
-                                size='xl' aria-label="Download preprocessing log"
-                                icon={<DownloadIcon />} /> </Text>
-
-                            <Box overflowY='scroll' height={200}>
-                                <Text style={{ whiteSpace: "pre-wrap" }}>
-                                    {response?.preprocessingLog}
-                                </Text>
+                            <Text mb="8px">
+                                Preprocessing Log
+                                <IconButton onClick={() => downloadOutput("preprocessing_log")} variant="ghost" size="xl" aria-label="Download preprocessing log" icon={<DownloadIcon />} />
+                            </Text>
+                            <Box overflowY="scroll" height={200}>
+                                <Text style={{ whiteSpace: "pre-wrap" }}>{response.preprocessingLog}</Text>
                             </Box>
                         </Box>
                         {response.errorLog ? (
                             <Box flex={1}>
-                                <Text mb='8px'>Error Log</Text>
-
-                                <Box overflowY='scroll' height={200}>
-                                    <Text style={{ whiteSpace: "pre-wrap" }}>
-                                        {response?.errorLog}
-                                    </Text>
+                                <Text mb="8px">Error Log</Text>
+                                <Box overflowY="scroll" height={200}>
+                                    <Text style={{ whiteSpace: "pre-wrap" }}>{response.errorLog}</Text>
                                 </Box>
                             </Box>
                         ) : (
                             <Box flex={1}>
-                                <Text mb='8px'>Output <IconButton onClick={() => downloadOutput("preprocessedText")} variant={'ghost'}
-                                    size='xl' aria-label="Download output"
-                                    icon={<DownloadIcon />} /> </Text>
-
-                                <Box overflowY='scroll' height={200}>
-                                    <Text style={{ whiteSpace: "pre-wrap" }}>
-                                        {response?.preprocessedText}
-                                    </Text>
+                                <Text mb="8px">
+                                    Output
+                                    <IconButton onClick={() => downloadOutput("preprocessedText")} variant="ghost" size="xl" aria-label="Download output" icon={<DownloadIcon />} />
+                                </Text>
+                                <Box overflowY="scroll" height={200}>
+                                    <Text style={{ whiteSpace: "pre-wrap" }}>{response.preprocessedText}</Text>
                                 </Box>
                             </Box>
                         )}
                     </Flex>
                     <Center>
-                        <Button onClick={() => setResponse(undefined)} mb={17} colorScheme="orange" variant='ghost'>Clear Logs</Button>
+                        <Button onClick={() => setResponse(undefined)} mb={17} colorScheme="orange" variant="ghost">Clear Logs</Button>
                     </Center>
                 </>
-           
             )}
+
             <Box mt={17} p={4} bg="gray.800" color="gray.500">
                 <Text fontSize="lg" mb={4} color="white">How to Use</Text>
                 <Text mb={2}>
-                    For detailed usage instructions, please visit the <Link href="https://github.com/Bikatr7/Kairyou" color="orange.400" isExternal>GitHub repository README</Link>.
+                    For detailed usage instructions, please visit the <Link href="https://github.com/Bikatr7/Kairyou" color="orange.400" isExternal
+
+>GitHub repository README</Link>.
                 </Text>
                 <Text mb={2}>
                     Kairyou is a tool for preprocessing Japanese text. You can upload a text file or input text directly, and provide a JSON file with replacement rules. The tool will preprocess the text according to the provided rules and return the results.
@@ -326,6 +278,17 @@ function KairyouPage()
                     3. Click "Submit" to preprocess the text.<br />
                     4. Review the preprocessing log and output, and download if necessary.<br />
                     5. If there are any errors, they will be displayed in the error log.
+                </Text>
+                <Text mt={2}>
+                    Please note that the Turnstile verification is required to use this tool. 
+                    This is in place to prevent abuse and ensure fair usage. 
+                    You must complete the verification for every submission.
+                </Text>
+                <Text mt={2}>
+                    The Kairyou endpoint access is provided for free here, but please be mindful of the usage and do not abuse the service.
+                </Text>
+                <Text mt={2}>
+                    If you wish to actually directly use the endpoint, We'd be interested, contact us at <Link href="mailto:contact@kakusui.org" color="orange.400">contact@kakusui.org</Link>.
                 </Text>
             </Box>
 
