f54c1124a930f640f56d8901bbe5545fe87d05fb
Kaden Bilyeu <Bikatr7@proton.me>
2024-09-08 04:20:40 -0600

    reoragnized backend dev ops (dockerfiles)

--- backend/entrypoint_dev.sh
+++ backend/entrypoint_dev.sh
@@ -4,8 +4,4 @@
 ## Use of this source code is governed by an GNU Affero General Public License v3.0
 ## license that can be found in the LICENSE file.
 
-## production
-exec uvicorn main:app --host 0.0.0.0 --port 8000 --workers 2 --limit-max-requests 25
-
-## development
-## exec uvicorn main:app --host 0.0.0.0 --port 5000 --reload
+exec uvicorn main:app --host 0.0.0.0 --port 5000 --reload
